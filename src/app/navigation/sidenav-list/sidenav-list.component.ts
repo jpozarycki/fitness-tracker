487a6efa599aee6a633e3c8cafa7a052c47ab268
jpozarycki <jakub.pozarycki@gmail.com>
2019-06-20 13:57:22 +0200

    Imlemented auth service, user model. Elements on navbar are displayed according to authenticate status.

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,18 +1,29 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {AuthService} from '../../auth/auth.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() closeSidenav = new EventEmitter<void>();
-  constructor() { }
+  auth$ = new Subscription();
+  isAuth: boolean;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.auth$ = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
   }
 
   onClose() {
     this.closeSidenav.emit();
+  }
+
+  ngOnDestroy(): void {
+    this.auth$.unsubscribe();
   }
 }
