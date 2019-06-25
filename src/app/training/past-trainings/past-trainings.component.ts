d1e16781ad34fb1deaba567b33a108ee496b8dc7
jpozarycki <jakub.pozarycki@gmail.com>
2019-06-25 09:30:50 +0200

    Implemented NgRx store training in past trainings component

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,27 +1,27 @@
-import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {Exercise} from '../exercise.model';
 import {TrainingService} from '../training.service';
-import {Subscription} from 'rxjs';
+import * as fromTraining from '../training.reducer';
+import {Store} from '@ngrx/store';
 
 @Component({
   selector: 'app-past-trainings',
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.css']
 })
-export class PastTrainingsComponent implements OnInit, AfterViewInit, OnDestroy {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
-  exercises$ = new Subscription();
 
   @ViewChild(MatSort, {static: false}) sort: MatSort;
   @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
 
-  constructor(private trainingService: TrainingService) {
+  constructor(private trainingService: TrainingService, private store: Store<fromTraining.State>) {
   }
 
   ngOnInit() {
-    this.exercises$ = this.trainingService.finishedExercisesChange.subscribe(exercises => {
+    this.store.select(fromTraining.getFinishedExercises).subscribe(exercises => {
       this.dataSource.data = exercises;
     });
     this.trainingService.fetchFinishedExercises();
@@ -36,7 +36,4 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit, OnDestroy 
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  ngOnDestroy(): void {
-    this.exercises$.unsubscribe();
-  }
 }
