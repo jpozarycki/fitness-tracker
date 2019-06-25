6d41a472b6df7d33aede0612a9726535cb506463
jpozarycki <jakub.pozarycki@gmail.com>
2019-06-25 13:51:26 +0200

    Added values for prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDDRsDW8qMXxV34xcmi9FHWIAxWiXTiUQ4',
+    authDomain: 'training-tracker-8bcff.firebaseapp.com',
+    databaseURL: 'https://training-tracker-8bcff.firebaseio.com',
+    projectId: 'training-tracker-8bcff',
+    storageBucket: 'training-tracker-8bcff.appspot.com',
+    messagingSenderId: '113452593829',
+    appId: '1:113452593829:web:70a682de70e11a33'
+  }
 };
